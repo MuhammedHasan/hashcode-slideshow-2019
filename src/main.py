90b93d98610982bb8e4f4f532b3754668c35bd81
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2019-02-28 20:38:42 +0100

    big fix

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ Image = namedtuple('Image', ['idx', 'shape', 'tags'])
 
 
 def sim(img1, img2):
+    intersect = img1.tags.intersection(img2.tags)
+    if intersect == 0:
+        return 0
+
     return min(len(img1.tags.intersection(img2.tags)),
                len(img1.tags.difference(img2.tags)),
                len(img2.tags.difference(img1.tags)))
@@ -19,29 +23,48 @@ def parse(filename):
         yield Image(idx=i, shape=shape, tags=tags)
 
 
+def max_image(image, images, image_set):
+    curr_image = -1
+    curr_max = -1
+    stop = 10
+
+    for i in image_set:
+        stop -= 1
+        if stop == 0:
+            break
+
+        alt_sim = sim(images[i], images[image])
+        if curr_max < alt_sim:
+            curr_image = i
+            curr_max = alt_sim
+
+    return curr_image
+
+    return max(image_set,
+               key=lambda x: sim(images[image], images[x]))
+
+
 def solve(images):
     '''
     Return list of tuple of images []
 
     '''
-
     images = list(filter(lambda x: x.shape == 'H', images))
+    image = images[0].idx
     images = {i.idx: i for i in images}
-
     image_set = set([i.idx for i in images.values()])
-    image = 0
+
+    i = 0
 
     while True:
+        i += 1
+        print(i, image)
         if len(image_set) > 1:
             image_set.remove(image)
-            next_image = max(image_set,
-                             key=lambda x: sim(images[image], images[x]))
+            next_image = max_image(image, images, image_set)
             yield (images[next_image], )
             image = next_image
         elif len(image_set) == 1:
-            yield (images[next_image], )
-            break
-        else:
             break
 
 
@@ -59,7 +82,7 @@ def write(solution, filename):
 if __name__ == "__main__":
     files = [
         'a_example',
-        'b_lovely_landscapes',
+        # 'b_lovely_landscapes',
         'c_memorable_moments',
         'd_pet_pictures',
         'e_shiny_selfies'
