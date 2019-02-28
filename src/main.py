7477abbd78b769ae7772c141a0ab951ef8921530
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2019-02-28 17:36:31 +0100

    init

---
+++ src/main.py
@@ -0,0 +1,22 @@
+def parse(filename):
+    pass
+
+
+def solve(obj):
+    pass
+
+
+def write(filename):
+    pass
+
+
+if __name__ == "__main__":
+    files = [
+        'small'
+        'big'
+    ]
+
+    for f in files:
+        obj = parse('inputs/%s.in' % f)
+        solution = solve(obj)
+        write(solution, 'outputs/%s.out' % f)
